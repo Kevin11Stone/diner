4a84ca07d4ec5a99913b1e19b0b877c0b6fc56bd
Kevin Stone <ruca6616@gmail.com>
2023-01-26 19:56:42 -0500

    created session to pass order to summary page

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+
+// start a session
+session_start();
+
+
 // require autoload file
 require_once('vendor/autoload.php');
 
@@ -35,6 +40,36 @@ $f3->route('GET /lunch', function () {
     // instantiate a view
     $view = new Template();
     echo $view->render("views/lunch.html");
+});
+
+// define a order-form1 route (328/diner/order-form1)
+$f3->route('GET|POST /order1', function ($f3) {
+
+    // if the form as been posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        // move data form POST to SESSION array
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+
+        // redirect to summary page
+        $f3->reroute('summary');
+
+    }
+
+
+    // instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-form1.html");
+});
+
+
+// define a order-summary route (328/diner/order-summary )
+$f3->route('GET /summary', function () {
+
+    // instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-summary.html");
 });
 
 
